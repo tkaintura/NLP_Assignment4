f49a5702357adef5c660ea956b050453d0df9174
tkaintura <tkaintura@gmail.com>
2023-05-18 14:09:40 +0200

    updated run.py

--- run.py
+++ run.py
@@ -52,7 +52,10 @@ def train(parser, train_data, dev_data, output_path, batch_size=1024, n_epochs=1
     ###     Adam Optimizer: https://pytorch.org/docs/stable/optim.html
     ###     Cross Entropy Loss: https://pytorch.org/docs/stable/nn.html#crossentropyloss
 
-
+    # Construct Adam optimizer
+    optimizer = optim.Adam(parser.model.parameters(), lr=lr)
+    # Construct the Cross Entropy Loss function with mean reduction
+    loss_func = nn.CrossEntropyLoss(reduction="mean")
 
     ### END YOUR CODE
 
@@ -105,8 +108,14 @@ def train_for_epoch(parser, train_data, dev_data, optimizer, loss_func, batch_si
             ### Please see the following docs for support:
             ###     Optimizer Step: https://pytorch.org/docs/stable/optim.html#optimizer-step
 
-
-
+            # Forward pass
+            logits = parser.model.forward(train_x)
+            # Compute loss
+            loss = loss_func(logits, train_y)
+            # Backward pass
+            loss.backward()
+            # Take step with optimizer
+            optimizer.step()
 
             ### END YOUR CODE
             prog.update(1)
